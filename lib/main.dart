2cb3daff4b4f2c154c7dadfb357fc9a211cff0f1
David Allen <davetvmusic@hotmail.com>
2022-02-04 18:32:30 +1000

    update file name

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pagination_app/Pagination.dart';
+import 'package:pagination_app/pagination.dart';
 
 void main() {
   runApp(ProviderScope(child: const MyApp()));
